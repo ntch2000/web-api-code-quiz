dd50fef044a712a7b1f9117384161fbabc0b3c64
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-09-27 22:25:23 -0400

    Added questions and answers to the questionBank array of objects

--- script.js
+++ script.js
@@ -26,24 +26,46 @@ timerDisplay.textContent = "Time: " + timerSeconds;
 
 var questionBank = [
   {
-    question: "This is a test question 1",
-    choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
-    answer: "answer 2",
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
   },
   {
-    question: "This is a test question 2",
-    choices: ["answer a", "answer b", "answer 3", "answer 2"],
-    answer: "answer a",
+    question: "The condition in an if/else statement is enclosed within _____.",
+    choices: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parenthesis",
+      "4. square brackets",
+    ],
+    answer: "3. parenthesis",
   },
   {
-    question: "This is a test question 3",
-    choices: ["answer 1", "answer c", "answer e", "answer 4"],
-    answer: "answer 4",
+    question: "Arrays in JavaScript can be used to store _____.",
+    choices: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    answer: "4. all of the above",
   },
   {
-    question: "This is a test question 4",
-    choices: ["answer 2", "answer a", "answer 7", "answer g"],
-    answer: "answer g",
+    question:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    answer: "3. quotes",
+  },
+  {
+    question:
+      "A very useful  tool used during development and debugging for printing content to the debugger is:",
+    choices: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log",
+    ],
+    answer: "4. console.log",
   },
 ];
 
